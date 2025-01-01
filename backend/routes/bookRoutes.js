55fe2d63c39265ee90699e311446e0da2e95eede
Sanuda Sandipa <sanudasandipa29@gmail.com>
2025-01-01 20:44:56 +0530

    new

--- backend/routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { searchBooks, addToFavorites } = require('../controllers/bookController');
-
-const router = express.Router();
-
-router.get('/search', searchBooks);
-router.post('/add-to-favorites', addToFavorites);
-
-module.exports = router;
